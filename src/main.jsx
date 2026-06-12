2f6ef0d77e85708298ff9d7974aa170ffe0379c6
Rayanalajlan <rayan.al.ajlan123@gmail.com>
2026-06-12 14:25:23 +0300

    --

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
+import "./styles.tokens.css";
 import "./styles.css";
 import "./styles.theme-fix.css";
+import "./styles.dark-system.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
